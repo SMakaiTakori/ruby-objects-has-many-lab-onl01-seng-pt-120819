d9b0a893e3781afdf6afc0d99abd968dda5e3a3c
S. Makai Takori <makait87@gmail.com>
2020-01-29 17:16:59 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -7,18 +7,17 @@ class Author
   def initialize(name)
     @name = name
     @posts = []
-    
   end  
   
   def add_post(post)
     self.posts << post
     post.author = self
-    @@post_count += 1
+    @@post_count +=1
   end
   
   def add_post_by_title(title)
     post = Post.new(title, self)
-  
+    @@post_count +=1
   end
   
   def posts
