b2652cac6d2a1c9b02f1dbd9f48bb473c00ad53c
S. Makai Takori <makait87@gmail.com>
2020-01-29 16:15:02 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,17 @@
 class Author
 
+  attr_accessor :name
 
+  def initialize(name)
+    @name = name
+  end  
   
-
-
+  def add_post(post)
+    
+  
+  end  
+  
+  def posts
+    @posts
+  end  
 end  
