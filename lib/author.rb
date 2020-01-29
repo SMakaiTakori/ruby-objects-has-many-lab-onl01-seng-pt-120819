376eb4d38fba424df8a914d3d23d58320f0e2243
S. Makai Takori <makait87@gmail.com>
2020-01-29 16:36:20 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -13,8 +13,19 @@ class Author
   end
   
   def add_post_by_title(title)
-    post = Post.new(title, self)  
+    post = Post.new(title, self)
+    
   end
+  
+  
+   def add_song_by_name(name)
+    song = Song.new(name)
+    self.songs << song
+    song.artist = self
+    @@song_count += 1
+  end  
+  
+  
   
   def posts
     Post.all.select {|post| post.author == self}
